cec5493b3d3e96371de2a1f19a102f5c5d8be692
jumanazarovogabek773@gmail.com <jumanazarovogabek773@gmail.com>
2026-07-21 13:57:59 +0500

    Disable Redis for Railway

--- src/redis/redis.js
+++ src/redis/redis.js
@@ -1,14 +1,14 @@
-const { createClient } = require("redis");
+// const { createClient } = require("redis");
 
-const redisClient = createClient({ url: "redis://127.0.0.1:6379" });
+// const redisClient = createClient({ url: "redis://127.0.0.1:6379" });
 
-redisClient.on("error", (err) => {
-    console.log("Redis Error:", err);
-});
+// redisClient.on("error", (err) => {
+//     console.log("Redis Error:", err);
+// });
 
-(async () => {
-    await redisClient.connect();
-    console.log("Redis Connected");
-})();
+// (async () => {
+//     await redisClient.connect();
+//     console.log("Redis Connected");
+// })();
 
-module.exports = redisClient;
+// module.exports = redisClient;
