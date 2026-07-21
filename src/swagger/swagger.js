9cd98acdbfa88c5fc528cf66e6730c5ab07370c4
jumanazarovogabek773@gmail.com <jumanazarovogabek773@gmail.com>
2026-07-21 15:28:56 +0500

    swagger.js

--- src/swagger/swagger.js
+++ src/swagger/swagger.js
@@ -1,36 +1,38 @@
 require("dotenv").config();
-const port = process.env.PORT || 5001;
 
 const swaggerOptions = {
     definition: {
-    openapi: "3.0.0",
-    info: {
-        title: "SkillUp API",
-        version: "1.0.0",
-        description: "SkillUp API hujjatlari",
-    },
-    servers: [
-        {
-            url: `http://localhost:${port}`,
+        openapi: "3.0.0",
+        info: {
+            title: "SkillUp API",
+            version: "1.0.0",
+            description: "SkillUp API hujjatlari",
         },
-    ],
-    components: {
-        securitySchemes: {
-            bearerAuth: {
-                type: "http",
-                scheme: "bearer",
-                bearerFormat: "JWT",
+
+        servers: [
+            {
+                url: process.env.API_URL || "http://localhost:5001",
+            },
+        ],
+
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: "http",
+                    scheme: "bearer",
+                    bearerFormat: "JWT",
+                },
             },
         },
-    },
-    security: [
-        {
-            bearerAuth: [],
-        },
-    ],
-},
-   apis: ["./src/routes/*.js"],
-};
 
+        security: [
+            {
+                bearerAuth: [],
+            },
+        ],
+    },
+
+    apis: ["./src/routes/*.js"],
+};
 
 module.exports = swaggerOptions;
