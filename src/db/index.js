e7812c204532e40402c197cdfbe0cd04873b998b
jumanazarovogabek773@gmail.com <jumanazarovogabek773@gmail.com>
2026-07-24 14:43:04 +0500

    Use Railway DATABASE_URL

--- src/db/index.js
+++ src/db/index.js
@@ -2,8 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-    connectionString:
-        process.env.DATABASE_URL || process.env.DATABASE_PUBLIC_URL,
+    connectionString: process.env.DATABASE_PUBLIC_URL,
     ssl: {
         rejectUnauthorized: false,
     },
