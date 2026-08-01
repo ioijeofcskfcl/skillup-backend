31f5eb09b5a2fb4adcf2066b475aab4b83ae0a9f
jumanazarovogabek773@gmail.com <jumanazarovogabek773@gmail.com>
2026-08-01 13:00:00 +0500

    Fix Railway database and redis configuration

--- src/redis/redis.js
+++ src/redis/redis.js
@@ -1,6 +1,6 @@
 const { createClient } = require("redis");
 
-const redisClient = createClient({ url: process.env.REDIS_PUBLIC_URL });
+const redisClient = createClient({ url: process.env.REDIS_URL });
 
 redisClient.on("error", (err) => {
     console.log("Redis Error:", err);
