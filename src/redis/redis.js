1f8050f52fbefda12c2ad88a7f32a0aef33ecb03
jumanazarovogabek773@gmail.com <jumanazarovogabek773@gmail.com>
2026-08-01 13:17:18 +0500

    redis.js

--- src/redis/redis.js
+++ src/redis/redis.js
@@ -1,6 +1,10 @@
 const { createClient } = require("redis");
 
-const redisClient = createClient({ url: process.env.REDIS_URL });
+console.log("REDIS_URL:", process.env.REDIS_URL);
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL,
+});
 
 redisClient.on("error", (err) => {
     console.log("Redis Error:", err);
@@ -10,5 +14,6 @@ redisClient.on("error", (err) => {
     await redisClient.connect();
     console.log("Redis Connected");
 })();
-
+console.log("REDIS_URL:", process.env.REDIS_URL);
+console.log("NODE_ENV:", process.env.NODE_ENV);
 module.exports = redisClient;
